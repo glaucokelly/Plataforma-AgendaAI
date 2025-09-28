bcdc3fa24e0e12d05e6212b46390cc59202974b8
Glauco Kelly <glaucokelly@icloud.com>
2025-09-28 04:05:15 -0300

    Create consulta.js

---
+++ backend/models/consulta.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const consultaSchema = new mongoose.Schema({
+    paciente: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Paciente',
+        required: true
+    },
+    profissional: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Profissional',
+        required: true
+    },
+    data:{
+        type: Date,
+        required: true
+    },
+    horario: {
+        type: String,
+        required: true
+    },
+    duracao: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Agendada', 'Confirmada', 'Cancelada', 'Realizada'],
+        default: 'Agendada'
+    },
+    observacoes: {
+        type: String,
+        default: ''
+    },
+    dataCadastro: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+consultaSchema.index({ paciente: 1, data: 1, horario: 1}, { unique: true});
+
+
+export default mongoose.model('Consulta', consultaSchema);
