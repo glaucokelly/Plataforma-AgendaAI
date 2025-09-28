c2532f0deade4b86d18fdcc0b9e152e903a1d1be
Glauco Kelly <glaucokelly@icloud.com>
2025-09-28 04:13:24 -0300

    Create profissional.js

---
+++ models/profissional.js
@@ -0,0 +1,48 @@
+// backend/models/profissional.js
+import mongoose from "mongoose";
+
+const profissionalSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    required: true
+  },
+  especialidade: {
+    type: String,
+    required: true
+  },
+  crm: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  contato: {
+    telefone: {
+      type: String,
+      required: true
+    },
+    email: String
+  },
+  diasAtendimento: [
+    {
+      diaSemana: {
+        type: Number,
+        required: true,
+        enum: [0, 1, 2, 3, 4, 5, 6] // 0 = Domingo ... 6 = Sábado
+      },
+      horaInicio: {
+        type: String,
+        required: true
+      },
+      horaFim: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  dataCadastro: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model('Profissional', profissionalSchema);
